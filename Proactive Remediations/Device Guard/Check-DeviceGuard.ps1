24d85347099cf9f9241e67087db0919f41165b39
Simon H <simon.ludvig.hakansson@outlook.com>
2022-07-04 14:59:21 +0200

    Update Check-DeviceGuard.ps1

--- Proactive Remediations/Device Guard/Check-DeviceGuard.ps1
+++ Proactive Remediations/Device Guard/Check-DeviceGuard.ps1
@@ -1,7 +1,7 @@
 <#
 
 .DESCRIPTION
-Proactive Remediation that detects if Credential Guard is configured and or running in the endpoint.
+Proactive Remediation detection that checks if Credential Guard is configured and or running in the endpoint.
 Based on: https://docs.microsoft.com/en-us/windows/security/threat-protection/device-guard/enable-virtualization-based-protection-of-code-integrity#securityservicesconfigured
 
 #>
