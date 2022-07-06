0acc185e57b841b110a673866e1f576364708293
Simon H <simon.ludvig.hakansson@outlook.com>
2022-07-06 19:21:08 +0200

    Update Remediate-CredentialGuard.ps1

--- Proactive Remediations/Credential Guard/Remediate-CredentialGuard.ps1
+++ Proactive Remediations/Credential Guard/Remediate-CredentialGuard.ps1
@@ -3,15 +3,9 @@
 .DESCRIPTION
 Proactive Remediation that remediates missing Credential Guard configuration and additional MDM-registry cleanup for correct MEM-reporting.
 Based on: https://docs.microsoft.com/en-us/windows/security/threat-protection/device-guard/enable-virtualization-based-protection-of-code-integrity#securityservicesconfigured
+Removes reporting values for MEM, then adds required Credential Guard configuration (UEFI-Lock Enabled).
 
 #>
-
-# Check status of Credential Guard in the registry
-$CGRegistry = "HKLM:\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\CredentialGuard"
-$CGEnabled = (Get-ItemProperty -Path $CGRegistry -Name "Enabled" -ErrorAction SilentlyContinue)
-$CGLocked = (Get-ItemProperty -Path $CGRegistry -Name "Locked" -ErrorAction SilentlyContinue)
-
-# If status is OK, do nothing. Else clean the reporting properties from HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\DeviceGuard and then enable Credential Guard with UEFI-Lock using registry values.
 try
 {
     $DGProperties = "HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\DeviceGuard"
@@ -22,11 +16,6 @@ try
     New-ItemProperty -Path "$DeviceGuardPath\Scenarios\CredentialGuard" -Name "Enabled" -PropertyType "DWORD" -Value 1 -Force
     New-ItemProperty -Path "$DeviceGuardPath\Scenarios\CredentialGuard" -Name "Locked" -PropertyType "DWORD" -Value 1 -Force
     New-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\LSA" -Name "LsaCfgFlags" -PropertyType "DWORD" -Value 1 -Force
-    if ($CGEnabled.Enabled -eq '1' -and $CGLocked.Locked -eq '1')
-    {
-    Write-Host "Successfully cleaned reporting values and enabled Credential Guard with UEFI-Lock in registry."
-    exit 0
-}
 else {
     Write-Host "Failed to remediate the status of Credential Guard..."
     exit 1
